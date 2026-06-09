6b3ad1dceac174f74b4d6936bed115887d890d17
Imdo714 <gusehdla@naver.com>
2026-06-09 18:41:38 +0900

    fix: CORS에 front Url 등록

--- queue_system_backend/src/main/kotlin/com/queue/global/config/WebConfig.kt
+++ queue_system_backend/src/main/kotlin/com/queue/global/config/WebConfig.kt
@@ -9,7 +9,10 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins(
+                "http://localhost:5173",
+                "https://queue-system-jet.vercel.app"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
